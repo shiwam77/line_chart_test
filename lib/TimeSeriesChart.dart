fedd32b4c1196e9674fe4dc0b2f992b114eb894b
shiwam77 <shiwamkan77@gmail.com>
2020-06-13 00:01:33 -0700

    Final Touch & project end..

--- lib/TimeSeriesChart.dart
+++ lib/TimeSeriesChart.dart
@@ -11,7 +11,7 @@ class TrackerTrends {
 
 class ItemDetailsPage extends StatefulWidget {
   @override
-  _ItemDetailsPageState createState() => new _ItemDetailsPageState();
+  _ItemDetailsPageState createState() => _ItemDetailsPageState();
 }
 
 class _ItemDetailsPageState extends State<ItemDetailsPage> {
@@ -19,16 +19,16 @@ class _ItemDetailsPageState extends State<ItemDetailsPage> {
   @override
   Widget build(BuildContext context) {
     final data = [
-      new TrackerTrends(DateTime.parse("2020-05-31T01:21:30.781702"), 5),
-      new TrackerTrends(DateTime.parse('2020-05-31T01:21:30.781702'), 25),
-      new TrackerTrends(DateTime.parse("2020-05-29T14:59:29.412717"), 100),
-      new TrackerTrends(DateTime.parse("2020-05-29T17:21:14.690870"), 75),
-      new TrackerTrends(DateTime.parse("2020-05-30T01:21:28.835976"), 400),
-      new TrackerTrends(DateTime.parse("2020-06-05T01:21:28.835976"), 267),
+      TrackerTrends(DateTime.parse("2020-05-31T01:21:30.781702"), 5),
+      TrackerTrends(DateTime.parse('2020-05-31T01:21:30.781702'), 25),
+      TrackerTrends(DateTime.parse("2020-05-29T14:59:29.412717"), 100),
+      TrackerTrends(DateTime.parse("2020-05-29T17:21:14.690870"), 75),
+      TrackerTrends(DateTime.parse("2020-05-30T01:21:28.835976"), 400),
+      TrackerTrends(DateTime.parse("2020-06-05T01:21:28.835976"), 267),
     ];
 
     var series = [
-      new Series<TrackerTrends, DateTime>(
+      Series<TrackerTrends, DateTime>(
         id: 'Likes',
         colorFn: (_, __) => MaterialPalette.blue.shadeDefault,
         domainFn: (TrackerTrends datas, _) => datas.time,
@@ -38,7 +38,7 @@ class _ItemDetailsPageState extends State<ItemDetailsPage> {
     ];
 
     return Scaffold(
-      appBar: new AppBar(title: new Text("Details")),
+      appBar: AppBar(title: Text("Details")),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: TimeSeriesChart(
