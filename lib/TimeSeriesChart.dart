e907bae797344b6e01c157575deec22915358c73
shiwam77 <shiwamkan77@gmail.com>
2020-06-12 23:58:07 -0700

    Final Touch & project end

--- lib/TimeSeriesChart.dart
+++ lib/TimeSeriesChart.dart
@@ -4,9 +4,9 @@ import 'package:flutter/material.dart';
 /// Sample time series data type.
 class TrackerTrends {
   final DateTime time;
-  final int TargetProgressValue;
+  final int targetProgressValue;
 
-  TrackerTrends(this.time, this.TargetProgressValue);
+  TrackerTrends(this.time, this.targetProgressValue);
 }
 
 class ItemDetailsPage extends StatefulWidget {
@@ -19,11 +19,12 @@ class _ItemDetailsPageState extends State<ItemDetailsPage> {
   @override
   Widget build(BuildContext context) {
     final data = [
-      new TrackerTrends(new DateTime(2017, 9, 19), 5),
-      new TrackerTrends(new DateTime(2017, 9, 26), 25),
-      new TrackerTrends(new DateTime(2017, 10, 3), 100),
-      new TrackerTrends(new DateTime(2017, 10, 10), 75),
-      new TrackerTrends(new DateTime(2017, 10, 15), 1000),
+      new TrackerTrends(DateTime.parse("2020-05-31T01:21:30.781702"), 5),
+      new TrackerTrends(DateTime.parse('2020-05-31T01:21:30.781702'), 25),
+      new TrackerTrends(DateTime.parse("2020-05-29T14:59:29.412717"), 100),
+      new TrackerTrends(DateTime.parse("2020-05-29T17:21:14.690870"), 75),
+      new TrackerTrends(DateTime.parse("2020-05-30T01:21:28.835976"), 400),
+      new TrackerTrends(DateTime.parse("2020-06-05T01:21:28.835976"), 267),
     ];
 
     var series = [
@@ -31,28 +32,35 @@ class _ItemDetailsPageState extends State<ItemDetailsPage> {
         id: 'Likes',
         colorFn: (_, __) => MaterialPalette.blue.shadeDefault,
         domainFn: (TrackerTrends datas, _) => datas.time,
-        measureFn: (TrackerTrends datas, _) => datas.TargetProgressValue,
+        measureFn: (TrackerTrends datas, _) => datas.targetProgressValue,
         data: data,
       )
     ];
 
-    var chart = new TimeSeriesChart(
-      series,
-      animate: true,
-    );
-
-    var chartWidget = new Padding(
-      padding: new EdgeInsets.all(32.0),
-      child: new SizedBox(
-        height: 200.0,
-        child: chart,
-      ),
-    );
-
     return Scaffold(
       appBar: new AppBar(title: new Text("Details")),
-      body: new Center(
-        child: chartWidget,
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: TimeSeriesChart(
+          series,
+          animate: true,
+          behaviors: [
+            ChartTitle("Like",
+                behaviorPosition: BehaviorPosition.top,
+                titleOutsideJustification: OutsideJustification.middleDrawArea),
+            ChartTitle("Date",
+                behaviorPosition: BehaviorPosition.bottom,
+                titleOutsideJustification: OutsideJustification.middleDrawArea),
+            ChartTitle("Progress",
+                behaviorPosition: BehaviorPosition.start,
+                titleOutsideJustification: OutsideJustification.middleDrawArea),
+            ChartTitle("Area",
+                behaviorPosition: BehaviorPosition.end,
+                titleOutsideJustification: OutsideJustification.middleDrawArea),
+          ],
+          defaultRenderer:
+              LineRendererConfig(includeArea: true, includePoints: true),
+        ),
       ),
     );
   }
